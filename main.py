8e3ce4023d876156540b93f925bbc1d4f375e293
EthanMaxwell <maxwellethan123@gmail.com>
2023-12-30 04:45:49 +1300

    Now prints out puzzle and solution

--- main.py
+++ main.py
@@ -63,6 +63,44 @@ try:
             
         known_letters.update({char for char in new_word})
         words.append(new_word)
-finally:        
-    print({starting_word[i] for i in starting_poses})
-    print(words)
+finally:  
+    starting_letter = {starting_word[i] for i in starting_poses}      
+    random_letters = random.sample(list(known_letters), len(known_letters))
+    random_words = random.sample(words, len(words))
+    
+    # Print out puzzle
+    print("Letters:")
+    for i in range(len(known_letters)):
+        print(str(i).zfill(2), end=" ")   
+    print()
+    for i in range(len(known_letters)):
+        if random_letters[i] in starting_letter:
+            print(random_letters[i], end="  ")
+        else:
+            print("__", end=" ")
+    
+    print("\nWords:")
+    
+    for word in random_words:
+        for i in range(len(word)):
+            print(str(random_letters.index(word[i])).zfill(2), end=" ")
+        print()
+        for i in range(len(word)):
+            if word[i] in starting_letter:
+                print(word[i], end="  ")
+            else:
+                print("__", end=" ")
+        print('\n')
+        
+    print('\n' *10 + "Solution:")
+    for i in range(len(known_letters)):
+        print(str(i).zfill(2), end=" ")   
+    print()
+    for i in range(len(known_letters)):
+        print(random_letters[i], end="  ")
+    print()
+    for word in random_words:
+        print(word + " " + str(words.index(word)))
+        
+    print(len(words))
+        
